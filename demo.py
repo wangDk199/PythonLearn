a615d7d0fcf3f93e16b11073e0f91cb5ac496fb5
wangDk199 <1091886414@qq.com>
2020-03-26 00:28:37 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,26 @@
+"""
+将华摄氏转换为摄氏度
+"""
+# f = float(input('请输入华摄氏：'));
+# c = (f - 32)/1.8;
+# print(c) 
+
+"""
+输入圆的半径计算圆的周长和面积
+"""
+# r = float(input('圆的半径为：'))
+# print("圆的周长：")
+# c = 2*r*3.14
+# print(c)
+# print("圆的面积：")
+# s = r*r*3.14
+# print(s)
+
+"""
+输入年份判断是不是闰年
+"""
+year = float(input('输入年份'));
+if((year % 4 == 0 and year % 100 == 0) or year % 400 == 0 ) :
+  print("year 是闰年")
+else :
+   print("year 是平年")
